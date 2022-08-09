deae01218bdb58146679d1b9b7b25abbad551f7f
Yaduttam95 <yaduttampareek95@gmail.com>
2022-08-09 07:55:46 +0530

    Added files via Upload

---
+++ Lists02.py
@@ -0,0 +1,11 @@
+# Date and Time
+# Program Created by Yaduttam95
+# Date: 8 Aug
+
+list1 = [1, "hi", "Python", 2]
+print(type(list1))  #=><class 'list'>
+print(list1)    #=>[1, 'hi', 'Python', 2]
+print(list1[3:])    #=>[2]
+print(list1[0:2])   #=>[1, 'hi']
+print(list1 + list1)    #=>[1, 'hi', 'Python', 2, 1, 'hi', 'Python', 2]
+print(list1 * 3)    #=>[1, 'hi', 'Python', 2, 1, 'hi', 'Python', 2, 1, 'hi', 'Python', 2]
